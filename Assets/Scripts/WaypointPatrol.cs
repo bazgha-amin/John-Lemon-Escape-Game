c721de00984e011f54404d4f570f6d3010ab6606
Bazgha Amin <bazgha_amin@yahoo.com>
2020-06-18 01:35:14 +0500

    Initial commit

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int m_CurrentWaypointIndex;
+
+//This line of code is declaring a public variable called waypoints, which is an array of Transforms. 
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+
+    }
+
+    void Update()
+    {
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination (waypoints[m_CurrentWaypointIndex].position);
+
+
+        }
+
+    }
+}
